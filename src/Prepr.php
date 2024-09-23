611263921d49d5171b5805ba8e84b10e2ecab7a4
Tom Swinkels <tomo_swinkels@hotmail.com>
2024-09-23 15:42:37 +0200

    Fix for chunk path

--- src/Prepr.php
+++ src/Prepr.php
@@ -253,14 +253,15 @@ class Prepr
 
         $assetId = data_get($start->getResponse(), 'id');
 
+        //Set the right url
+        $this->path($this->path . '/'.$assetId);
+        
         for ($i = 0; $i <= $chunks; $i++) {
             $offset = ($this->chunkSize * $i);
             $endOfFile = $i === $chunks;
             $limit = ($endOfFile ? ($fileSize - $offset) : $this->chunkSize);
 
             $stream = new LimitStream($original, $limit, $offset);
-
-            $this->path($this->path . '/'.$assetId);
 
             $this->params = [
                 'upload_phase' => 'transfer',
